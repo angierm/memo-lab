9853a2ef90bee0dd7e7e837c4a325d8d81e134f0
angierm <eus_angie@hotmail.com>
2017-11-28 21:33:06 +0000

    se agregaron imagenes, efecto y rotacion 180 grados

---
+++ app.js
@@ -0,0 +1,17 @@
+var cards = document.getElementsByClassName("card");
+console.log(cards);
+for (var i = 0;i < cards.length; i++){
+var item= cards[i];
+ //console.log (cards[i]);
+item.classList.add("efects")
+item.addEventListener("mouseover",rotation);
+}
+
+function rotation () {
+ if (this.classList.contains("rotation-efect")){
+   this.classList.remove("rotation-efect");
+ }
+ else {
+   this.classList.add("rotation-efect");
+ }
+}
